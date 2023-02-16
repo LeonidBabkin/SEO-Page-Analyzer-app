ecb5be919eff236a2f1e6c8a1bf3bcc2de0ab679
LeonidBabkin <Babkinleo@gmail.com>
2023-02-16 15:39:40 +0300

    app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template, request
 import os
-import psycopg2
 from dotenv import load_dotenv
-from psycopg2.extras import NamedTupleCursor
-import datetime
 
 
 load_dotenv()
@@ -12,9 +9,6 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
-
-
-conn = psycopg2.connect('postgresql://leonid:babkin36@host:5432/page_analyzer')
 
 
 @app.route("/")
@@ -27,8 +21,4 @@ def hello_template():
 def hello_url():
     datum = request.form.to_dict()
     url = datum['url']
-    with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
-        cusr.execute("INSERT INTO urls (name, created_at) VALUES (%s, %s)", (site_url, datetime.time))
-    cursor.close()
-    conn.close()
     return render_template('page.html', url=url)
