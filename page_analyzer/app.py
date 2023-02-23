b13538de955bb1f9de295be8dc90ca3b1d193d83
LeonidBabkin <Babkinleo@gmail.com>
2023-02-23 14:03:09 +0300

    lst_urls.html

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -49,37 +49,46 @@ def hello_template():
     return render_template('index.html', data=data, messages=messages)
 
 
-@app.route('/urls', methods=['POST'])
+@app.route('/urls', methods=['GET', 'POST'])
 def hello_url():
-    datum = request.form.to_dict()
+    if request.method == 'POST':
+        datum = request.form.to_dict()
 
-    if datum['url'] == '':
-        flash('URL обязателен', 'error')
-        return redirect(url_for('hello_template'))
+        if datum['url'] == '':
+            flash('URL обязателен', 'error')
+            return redirect(url_for('hello_template'))
 
-    name, date = url_entry(datum)
-    valid_res = validate_url(name)
+        name, date = url_entry(datum)
+        valid_res = validate_url(name)
 
-    if valid_res is not None:
-        flash(valid_res, 'error')
-        return redirect(url_for('hello_template'))
+        if valid_res is not None:
+            flash(valid_res, 'error')
+            return redirect(url_for('hello_template'))
 
-    entry = check_uniqueness(name)
-    if entry is not None:
-        flash('Страница уже существует', 'success')
-        return redirect(url_for('get_url', id=entry[0]))
+        entry = check_uniqueness(name)
+        if entry is not None:
+            flash('Страница уже существует', 'success')
+            return redirect(url_for('get_url', id=entry[0]))
 
-    if entry is None:
+        if entry is None:
+            conn = psycopg2.connect(DATABASE_URL)
+            cur = conn.cursor(cursor_factory=NamedTupleCursor)
+            cur.execute("INSERT INTO urls (name, created_at) VALUES (%s, %s)",
+                        (name, date))
+            conn.commit()
+            cur.close()
+            conn.close()
+            entry = check_uniqueness(name)
+            flash('Страница успешно добавлена', 'success')
+            return redirect(url_for('get_url', id=entry[0]))
+    if request.method == 'GET':
         conn = psycopg2.connect(DATABASE_URL)
         cur = conn.cursor(cursor_factory=NamedTupleCursor)
-        cur.execute("INSERT INTO urls (name, created_at) VALUES (%s, %s)",
-                    (name, date))
-        conn.commit()
+        cur.execute("SELECT * FROM urls ORDER BY id DESC")
+        records = cur.fetchall()
         cur.close()
         conn.close()
-        entry = check_uniqueness(name)
-        flash('Страница успешно добавлена', 'success')
-        return redirect(url_for('get_url', id=entry[0]))
+        return render_template('lst_urls.html', records=records)
 
 
 @app.route('/urls/<int:id>')
