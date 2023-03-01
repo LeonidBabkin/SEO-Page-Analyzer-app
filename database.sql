39ece797c238dc28585c9da45179489f6b83a37c
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-01 15:05:40 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -6,11 +6,11 @@ CREATE TABLE urls (
 
 
 CREATE TABLE url_checks (
-	 id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-	 url_id bigint REFERENCES urls (id),
-	 status_code smallint,
-	 h1 varchar(255),
-	 title varchar(255),
-	 description varchar(255),
-	 created_at date
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id bigint REFERENCES urls (id),
+    status_code integer,
+    h1 text,
+    title text,
+    description text,
+    created_at date
 );
