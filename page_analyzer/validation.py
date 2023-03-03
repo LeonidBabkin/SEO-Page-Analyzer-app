1b70f9274896f962ae5ffe7b248d6680b0f9f9ff
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-03 12:58:06 +0300

    Update validation.py

--- page_analyzer/validation.py
+++ page_analyzer/validation.py
@@ -1,16 +1,5 @@
-import re
-regex = re.compile(r'^(?:http|ftp)s?://'  # http:// or https://
-                   r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?'\
-                   '\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
-                   r'localhost|'  # localhost...
-                   r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
-                   r'(?::\d+)?'  # optional port
-                   r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+from validators.url import url
 
 
-def validate_url(url):
-    check = re.match(regex, url)
-    if check is None or len(check.group(0)) > 255:
-        return 'Некорректный URL'
-    else:
-        pass
+def validate_url(site_url):
+    return not url(site_url) or len(site_url) > 255
