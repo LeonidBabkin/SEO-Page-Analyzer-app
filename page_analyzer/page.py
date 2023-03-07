c5895ddd5a0eb021cb3145e5a6aaafb436d45338
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-07 14:35:06 +0300

    Update page.py

--- page_analyzer/page.py
+++ page_analyzer/page.py
@@ -22,3 +22,15 @@ def get_data_bits(url):
     except Exception:
         title = ''
     return status, title, h1, description
+
+
+def insert_into_url_checks(id, status_code, title, h1, description):
+    date = datetime.now().strftime("%Y-%m-%d")
+    conn = psycopg2.connect(DATABASE_URL)
+    with conn:
+        with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
+            cursor.execute("INSERT INTO url_checks (url_id, status_code,"
+                           " title, h1, description, created_at) VALUES"
+                           "(%s, %s, %s, %s,  %s, %s)",
+                           (id, status_code, title, h1, description, date))
+            conn.commit()
