79f5b267f9013c01bb1f26efa55aea5230a49860
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-07 14:50:39 +0300

    Update page.py

--- page_analyzer/page.py
+++ page_analyzer/page.py
@@ -3,6 +3,11 @@ import psycopg2
 from datetime import datetime
 from psycopg2.extras import NamedTupleCursor
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 def get_data_bits(url):
