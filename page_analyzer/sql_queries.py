bdd55b24c7217a4941de7120d6fdf8ac8f9a2b41
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-16 12:42:34 +0300

    Update sql_queries.py

--- page_analyzer/sql_queries.py
+++ page_analyzer/sql_queries.py
@@ -32,9 +32,9 @@ def select_all_sites():
                            " urls.id = checks.url_id ORDER BY id DESC;")
             site_list = cursor.fetchall()
             return site_list
-        
-        
-def select_certain_site():
+  
+ 
+def select_certain_site(site_url):
     conn = psycopg2.connect(DATABASE_URL)
     with conn:
         with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
