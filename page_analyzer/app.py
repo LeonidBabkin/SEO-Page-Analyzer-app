cc529b2d4170d52be53738bf065c2a084fb169b0
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-16 14:44:30 +0300

    Update app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -8,7 +8,6 @@ import psycopg2
 import requests
 from validators.url import url
 from psycopg2.extras import NamedTupleCursor
-from datetime import datetime
 from urllib.parse import urlparse
 from dotenv import load_dotenv
 from page_analyzer.page import get_data_bits
