b069725e75d204e8ba9081bcf8e51680df493f6e
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-16 11:37:30 +0300

    Update sql_queries.py

--- page_analyzer/sql_queries.py
+++ page_analyzer/sql_queries.py
@@ -1,11 +1,21 @@
 import os
-import requests
 import psycopg2
 from datetime import datetime
 from psycopg2.extras import NamedTupleCursor
-from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+def insert_into_url_checks(id, status_code, title, h1, description):
+    date = datetime.now().strftime("%Y-%m-%d")
+    conn = psycopg2.connect(DATABASE_URL)
+    with conn:
+        with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
+            cursor.execute("INSERT INTO url_checks (url_id, status_code,"
+                           " title, h1, description, created_at) VALUES"
+                           "(%s, %s, %s, %s,  %s, %s)",
+                           (id, status_code, title, h1, description, date))
+            conn.commit()
